65a9430f96f6ecbed262f68d6ade5ad80f98f1e2
elenag518 <patty518@yahoo.com>
2023-06-13 16:08:37 -0600

    allow mergedParams; add error method

--- src/pastes/pastes.router.js
+++ src/pastes/pastes.router.js
@@ -1,13 +1,16 @@
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
 const controller = require("./pastes.controller");
-
-router.route("/")
-.get(controller.list)
-.post(controller.create);
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/:pasteId")
 .get(controller.read)
 .put(controller.update)
-.delete(controller.delete);
+.delete(controller.delete)
+.all(methodNotAllowed);
+
+router.route("/")
+.get(controller.list)
+.post(controller.create)
+.all(methodNotAllowed);
 
 module.exports = router;
